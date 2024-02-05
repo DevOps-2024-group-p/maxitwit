b61b371b28d4d83af50cd1b9c7e59ac57989bc60
cemn <127931228+fridge7809@users.noreply.github.com>
2024-02-05 16:42:08 +0100

    modify compile tool to output bin to seperate dir

--- compileflagtool.sh
+++ compileflagtool.sh
@@ -1,7 +1,11 @@
 #!/bin/sh	
-command=$(gcc -o flag_tool flag_tool.c -lsqlite3)
+
+mkdir -p out
+
+command=$(gcc -o out/flag_tool flag_tool.c -lsqlite3)
+
 if [[ command ]]; then
-	echo "Compile ok"
+	echo "Compile ok, flag tool is located in /out"
 else
-	echo "Did not compile"
+	echo "Compile failed"
 fi
