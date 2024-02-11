cf51054fdb17c2aa39a43919b406235220349c46
cemn <127931228+fridge7809@users.noreply.github.com>
2024-02-11 17:08:51 +0100

    restore old artifacts we still need

---
+++ .old/compileflagtool.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+mkdir -p out
+
+command=$(gcc -o out/flag_tool flag_tool.c -lsqlite3)
+
+db=/tmp/minitwit.db
+
+
+if [ ! -f "/tmp/minitwit.db" ]; then
+	echo "no database, expected db at path $db"
+	exit 1;
+fi
+
+
+if [[ command ]]; then
+	echo "Compile ok, flag tool is located in /out"
+else
+	echo "Compile failed"
+fi
