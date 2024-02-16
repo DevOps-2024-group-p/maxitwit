a7bf2d62612bf4bce0f27b69c3e19e75bb14f471
Bence <benceluzsinszky@gmail.com>
2024-02-16 13:19:30 +0100

    added all old files to .old

---
+++ .old/ReadMe.md
@@ -0,0 +1,9 @@
+# This is the first Readme
+
+Project Members:
+    - AAFredsted
+    - Christian :)
+    - Bence ffs
+
+
+    yoyooy
--- .old/compileflagtool.sh
+++ .old/compileflagtool.sh
@@ -1,20 +1,7 @@
-#!/usr/bin/env bash
-
-mkdir -p out
-
-command=$(gcc -o out/flag_tool flag_tool.c -lsqlite3)
-
-db=/tmp/minitwit.db
-
-
-if [ ! -f "/tmp/minitwit.db" ]; then
-	echo "no database, expected db at path $db"
-	exit 1;
-fi
-
-
+#!/bin/sh	
+command=$(gcc -o flag_tool flag_tool.c -lsqlite3)
 if [[ command ]]; then
-	echo "Compile ok, flag tool is located in /out"
+	echo "Compile ok"
 else
-	echo "Compile failed"
+	echo "Did not compile"
 fi
