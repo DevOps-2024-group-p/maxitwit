0cdf62f9ddfcc729ba37989881a153d434c3cde9
Bence <benceluzsinszky@gmail.com>
2024-02-23 15:44:16 +0100

    made flashes work

--- src/app.js
+++ src/app.js
@@ -17,12 +17,12 @@ const flash = require('connect-flash')
 const database = require('../db/database')
 
 database.initSchema()
-  .then(() => {
-    console.log('Database schema initialized successfully.')
-  })
-  .catch((err) => {
-    console.error('Error initializing database schema:', err)
-  })
+	.then(() => {
+		console.log('Database schema initialized successfully.')
+	})
+	.catch((err) => {
+		console.error('Error initializing database schema:', err)
+	})
 
 // Import routers for different paths
 const loginRouter = require('./routes/login') // Router for login related paths
@@ -41,52 +41,58 @@ app.set('view engine', 'pug') // Sets Jade (now Pug) as the template engine for 
 // Middleware setup
 // middleware only used during development
 if (process.env.NODE_ENV === 'dev') {
-  const logger = require('morgan') // http request logger middleware for node.js
-  app.use(logger('dev')) // Use Morgan to log requests to the console in 'dev' format, which includes method, url, status, response time
+	const logger = require('morgan') // http request logger middleware for node.js
+	app.use(logger('dev')) // Use Morgan to log requests to the console in 'dev' format, which includes method, url, status, response time
 }
 // middleware for use in production environment
 app.use(express.json()) // Parses incoming requests with JSON payloads, making it easy to handle JSON data
 app.use(express.urlencoded({ extended: false })) // Parses incoming requests with URL-encoded payloads, useful for form submissions
 app.use(cookieParser()) // Parse Cookie header and populate req.cookies with an object keyed by cookie names
 app.use(express.static(path.join(__dirname, 'public'))) // Serve static files (images, CSS, JavaScript) from the 'public' directory
-app.use(flash())
+
 app.use(session({
-  resave: false,
-  saveUninitialized: false,
-  store: new SQLiteStore({
-    dir: './db',
-    db: 'sessions.db'
-  }),
-  secret: 'your secret',
-  cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 } // 1 week
+	resave: false,
+	saveUninitialized: true,
+	store: new SQLiteStore({
+		dir: './db',
+		db: 'sessions.db'
+	}),
+	secret: 'your secret',
+	cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 } // 1 week
 }))
 
-if (process.env.NODE_ENV === 'api') {
-  app.use('/', apiRouter) // Use the API router for requests to '/api'
-} else {
-  app.use('/login', loginRouter) // Use the login router for requests to '/login'
-  app.use('/logout', logoutRouter) // Use the logout router for requests to '/login'
-  app.use('/register', registerRouter) // Use the register router for requests to '/register'
-  app.use('/', timelineRouter) // Use the public timeline router for requests to '/'
-}
-// Error handler middleware
-app.use((err, req, res, next) => {
-  // Set locals, providing error details only in development environment
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-  console.log(process.env.NODE_ENV)
-
-
-  // Render the error page, setting the status code
-  res.status(err.status || 500)
-  res.render('error') // Uses the view engine to render the error page
-})
+app.use(flash())
 
 app.use((req, res, next) => {
-  res.success_messages = req.flash('success')
-  res.error_messages = req.flash('error')
-  next()
+	res.locals.success_messages = req.flash('success')
+	res.locals.error_messages = req.flash('error')
+	next()
 })
+
+
+if (process.env.NODE_ENV === 'api') {
+	app.use('/', apiRouter) // Use the API router for requests to '/api'
+} else {
+	app.use('/login', loginRouter) // Use the login router for requests to '/login'
+	app.use('/logout', logoutRouter) // Use the logout router for requests to '/login'
+	app.use('/register', registerRouter) // Use the register router for requests to '/register'
+	app.use('/', timelineRouter) // Use the public timeline router for requests to '/'
+}
+
+// Error handler middleware
+app.use((err, req, res, next) => {
+	// Set locals, providing error details only in development environment
+	res.locals.message = err.message
+	res.locals.error = req.app.get('env') === 'development' ? err : {}
+	console.log(process.env.NODE_ENV)
+
+
+	// Render the error page, setting the status code
+	res.status(err.status || 500)
+	res.render('error') // Uses the view engine to render the error page
+})
+
+
 
 // Export the app for use by other modules (like the server starter script)
 module.exports = app
