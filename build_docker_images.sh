b910620f224a8c1de6a18888d5faf31b66babd09
cemn <127931228+fridge7809@users.noreply.github.com>
2024-03-05 14:26:16 +0100

    ci: add workflow
    re #9

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -3,7 +3,7 @@
 echo "Building Minitwit Images"
 docker build -t $DOCKER_USERNAME/maxitwitserver -f Dockerfile.app .
 docker build -t $DOCKER_USERNAME/maxitwitapi -f Dockerfile.api .
-docker build -t $DOCKER_USERNAME/maxitwittest -f Dockerfile.api .
+docker build -t $DOCKER_USERNAME/maxitwittest -f Dockerfile.test .
 
 echo "Login to Dockerhub, provide your password below..."
 read -s DOCKER_PASSWORD
