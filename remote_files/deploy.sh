b2ccbef14c34517e79bace1bdb2ba7f5cc8429fe
christian <127931228+fridge7809@users.noreply.github.com>
2024-03-14 21:29:13 +0100

    fix(api): fix bug (#41)

--- remote_files/deploy.sh
+++ remote_files/deploy.sh
@@ -1,6 +1,6 @@
 source ~/.bash_profile
 
-cd /maxitwit/remote_files
+cd /vagrant/remote_files
 
 docker compose -f compose.yaml pull
 docker compose -f compose.yaml up -d
--- src/routes/api.js
+++ src/routes/api.js
@@ -21,7 +21,7 @@ const logIncomingRequests = (req, res, next) => {
   next()
 }
 
-function formatMessagesAsJSON (messages) {
+function formatMessagesAsJSON(messages) {
   const formattedMessages = []
   messages.forEach((msg) => {
     const formatted = {}
@@ -33,7 +33,7 @@ function formatMessagesAsJSON (messages) {
   return formattedMessages
 }
 
-function updateLatest (req) {
+function updateLatest(req) {
   const { latest } = req.query
   if (!latest) {
     return
@@ -45,7 +45,7 @@ function updateLatest (req) {
   })
 }
 
-function getLatest () {
+function getLatest() {
   const fileContent = fs.readFileSync('latest.txt', 'utf8')
   const parsed = fileContent.trim().split('\n')
   const last = parsed[parsed.length - 1]
@@ -158,7 +158,6 @@ router.all('/fllws/:username', logIncomingRequests, requireRequestFromSimulator,
     updateLatest(req)
     const { username } = req.params
     const action = req.body
-    const { no } = req.query
     const whoId = await userService.getUserIdByUsername(username)
     if (!whoId) {
       return res.status(404).send()
@@ -179,7 +178,7 @@ router.all('/fllws/:username', logIncomingRequests, requireRequestFromSimulator,
       await userService.unfollowUser(whoId.user_id, whomId.user_id)
       return res.status(204).send()
     }
-    const followed = await userService.getAllFollowedUsers(whoId.user_id, no)
+    const followed = await userService.getAllFollowedUsers(whoId.user_id)
     return res.json({ follows: JSON.stringify(followed) })
   } catch (err) {
     console.error(err)
