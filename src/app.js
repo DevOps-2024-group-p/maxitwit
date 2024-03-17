62fb548b903b247bda9aaf58e9d30a99d5b3354e
cemn <127931228+fridge7809@users.noreply.github.com>
2024-03-17 15:22:14 +0100

    fix: revert app.js such that sessions are handled as they were before

--- src/app.js
+++ src/app.js
@@ -5,60 +5,56 @@
  */
 
 // Import necessary modules
-require('dotenv').config()
-const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
+require('dotenv').config() // Load environment variables from a .env file into process.env
+const express = require('express') // The main Express framework
+const path = require('path') // Core Node.js module to handle and transform file paths
+const cookieParser = require('cookie-parser') // Middleware to parse and set cookies in request objects
 const session = require('express-session')
+const SQLiteStore = require('connect-sqlite3')(session)
 const flash = require('connect-flash')
-const PgSession = require('connect-pg-simple')(session)
-const { Pool } = require('pg')
 
 // Import routers for different paths
-const loginRouter = require('./routes/login')
-const logoutRouter = require('./routes/logout')
-const registerRouter = require('./routes/register')
-const timelineRouter = require('./routes/timeline')
-const apiRouter = require('./routes/api')
+const loginRouter = require('./routes/login') // Router for login related paths
+const logoutRouter = require('./routes/logout') // Router for logout related paths
+const registerRouter = require('./routes/register') // Router for register related paths
+const timelineRouter = require('./routes/timeline') // Router for public timeline related paths
+const apiRouter = require('./routes/api') // Router for public timeline related paths
 
 // Initialize the Express application
 const app = express()
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views')) // Specifies the directory where the Jade template files are located
+app.set('view engine', 'pug') // Sets Jade (now Pug) as the template engine for rendering views
 
 // Middleware setup
+// middleware only used during development
 if (process.env.NODE_ENV === 'development') {
-  const logger = require('morgan')
-  app.use(logger('dev'))
+  const logger = require('morgan') // http request logger middleware for node.js
+  app.use(logger('dev')) // Use Morgan to log requests to the console in 'dev' format, which includes method, url, status, response time
 }
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-
-const { SESSION_SECRET, DATABASE_URL } = process.env
+// middleware for use in production environment
+app.use(express.json()) // Parses incoming requests with JSON payloads, making it easy to handle JSON data
+app.use(express.urlencoded({ extended: false })) // Parses incoming requests with URL-encoded payloads, useful for form submissions
+app.use(cookieParser()) // Parse Cookie header and populate req.cookies with an object keyed by cookie names
+app.use(express.static(path.join(__dirname, 'public'))) // Serve static files (images, CSS, JavaScript) from the 'public' directory
+const { SESSION_SECRET } = process.env
 if (!SESSION_SECRET) {
   throw new Error('SESSION_SECRET is not set')
 }
 
-const pool = new Pool({
-  connectionString: DATABASE_URL
-})
-
 app.use(session({
-  store: new PgSession({
-    pool, // Connection pool
-    tableName: 'userSession'
-  }),
-  secret: SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
+  store: new SQLiteStore({
+    dir: './db',
+    db: 'sessions.db'
+  }),
+  secret: SESSION_SECRET,
   cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 } // 1 week
 }))
 
 app.use(flash())
+
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success')
   res.locals.error_messages = req.flash('error')
