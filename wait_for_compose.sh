3121bc7fe1138272d9f997546ece591c5a1fb8c2
Bence <benceluzsinszky@gmail.com>
2024-03-19 16:09:24 +0100

    fix(wait.sh): fixed wait.sh

--- wait_for_compose.sh
+++ wait_for_compose.sh
@@ -9,11 +9,15 @@ for i in {1..36}
 do
     if $(curl --output /dev/null --silent --head --fail "http://localhost:3000/"); then
         printf 'Server is up!\n'
-        exec $cmd
+        break
     fi
     printf 'Waiting for servers to be available\n'
     sleep 5
 done
 
-printf 'Server did not become available after 3 minutes\n'
-exit 1
+if [ $i -eq 36 ]; then
+    printf 'Server did not become available after 3 minutes\n'
+    exit 1
+fi
+
+# Continue with the rest of the script...
